348110e658a988bc39a4aecf60c58315363ffce0
paksasha76 <pak.aleksandr2001@mail.ru>
2023-06-27 23:25:10 +0300

    project

--- src/components/App.js
+++ src/components/App.js
@@ -34,6 +34,8 @@ function App() {
     setEditAvatar(false);
     setEditProfile(false);
     setAddPlace(false);
+    setSelectedCard(null);
+    setIsDeletePopup(false);
   }
 
   return (
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,11 +1,11 @@
-function ImagePopup({ card, onClose }) {
+function ImagePopup(props) {
   return (
-    <div class={`popup popup_type_view ${card ? "popup_opened" : ""}`}>
+    <div class={`popup popup_type_view ${props.card ? "popup_opened" : ""}`}>
       <div class="popup__zoom-view">
-        <button class="popup__close-btn" type="button"></button>
-        <img class="popup__zoom-image"   src={card ? card.link : "#"}
-            alt={card ? card.name : "#"} />
-        <p class="popup__zoom-title">{card ? card.name : ""}</p>
+        <button class="popup__close-btn" type="button" onClick={props.onClose}></button>
+        <img class="popup__zoom-image"   src={props.card ? props.card.link : "#"}
+            alt={props.card ? props.card.name : "#"} />
+        <p class="popup__zoom-title">{props.card ? props.card.name : ""}</p>
       </div>
     </div>
   );
